75a040e20be80f8e2ce82be81b8d8390e31ac9e4
Paul Smith <paul.smith@communicatorcorp.com>
2018-02-21 22:07:13 +0000

    Initial commit

---
+++ Puppeteer/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace Puppeteer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly INodeServices _nodeServices;
+
+        public HomeController(INodeServices nodeServices)
+        {
+            _nodeServices = nodeServices;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _nodeServices.InvokeAsync<string>("./Node/pdf.js", "<h1>title</h1>");
+            return Content(result) ;
+        }
+    }
+}
---
+++ Puppeteer/Node/pdf.js
@@ -0,0 +1,17 @@
+﻿const puppeteer = require('puppeteer');
+
+module.exports = function (callback, html) {
+
+    puppeteer.launch({
+        headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }).then(browser => {
+        browser.newPage()
+            .then(page => page.goTo("https://www.google.com"))
+            .then(response => response.pdf({ path: 'c:\hn.pdf', format: 'A4' }));
+    });
+    
+    //page.goTo("https://www.google.com");
+    //page.pdf({ path: 'hn.pdf', format: 'A4' });
+
+    callback(/* error */ null, html);
+};
