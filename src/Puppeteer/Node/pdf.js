3009278971eb25b44c6b83783c8c6fa0d378a400
Paul Smith <paul.smith@communicatorcorp.com>
2018-02-27 11:56:27 +0000

    paged pdfs

--- src/Puppeteer/Node/pdf.js
+++ src/Puppeteer/Node/pdf.js
@@ -2,9 +2,30 @@
 const tmp = require("tmp");
 const fs = require('fs');
 const fileUrl = require('file-url');
+const PDFMerge = require('pdf-merge');
+const path = require('path');
 
 module.exports = async function (result, html) {
 
+    const htmlFileName = await writeFile(html);
+
+    const pdfFiles = [
+        path.join(__dirname, '..', 'PdfContent', 'front.html'),
+        htmlFileName,
+        path.join(__dirname, '..', 'PdfContent', 'back.html')
+    ];
+
+    const browser = await createBrowser();
+    
+    const pdfPaths = await createPdfs(browser, pdfFiles);
+    
+    await browser.close();
+
+    PDFMerge(pdfPaths, { output: 'Stream' })
+        .then((stream) => { stream.pipe(result.stream); });
+};
+
+async function writeFile(html) {
     var tempHtmlFile = tmp.fileSync({
         postfix: ".html"
     });
@@ -13,18 +34,33 @@ module.exports = async function (result, html) {
         if (err) throw err;
         console.log('The file has been saved!');
     });
-    
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+
+    return tempHtmlFile.name;
+}
+
+async function createBrowser() {
+    return await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+}
+
+function createPdfs(browser, pages) {
+    return Promise.all(
+        pages.map((filePath) =>
+            createPdf(browser, filePath)
+        )
+    );
+}
+
+async function createPdf(browser, fileName) {
     const page = await browser.newPage();
 
-    await page.goto(fileUrl(tempHtmlFile.name), {
+    await page.goto(fileUrl(fileName), {
         waitUntil: 'networkidle2'
     });
 
-    var name = tmp.tmpNameSync() + ".pdf";
+    const tmpPdfFileName = tmp.tmpNameSync() + ".pdf";
 
     await page.pdf({
-        path: name,
+        path: tmpPdfFileName,
         format: 'A4',
         margin: {
             top: 0,
@@ -34,8 +70,7 @@ module.exports = async function (result, html) {
         }
     });
 
-    await browser.close();
+    await page.close();
 
-    var read = fs.createReadStream(name);
-    read.pipe(result.stream);
-};
+    return tmpPdfFileName;
+}
